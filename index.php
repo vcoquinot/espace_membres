aa30ad4838341744d67e1ebe16b3ece56bed1fcc
vcoquinot <vcoquinot@gmail.com>
2017-06-21 21:37:03 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Espace membres Nataraj Bollywood</title>
+    <html lang="fr">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+      <!-- Bootstrap core CSS -->
+    <link href="dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="starter-template.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="main.css">
+     <link href="https://fonts.googleapis.com/css?family=Kalam" rel="stylesheet">
+</head>
+
+<body>
+
+<!--INTRO PAGE MEMBRES-->
+<div class="container-fluid">
+      <header class="jumbotron row text-center" id="membres">
+
+            <h4><strong>Bienvenue sur votre espace membres.</strong></h4>
+            <p>Accédez aux vidéos de vos chorés en cliquant sur le cours auquel vous êtes inscrit</p>
+        </header>
+    </div>
+
+
+<!--LISTE DES GROUPES ET COURS-->
+    <div class="container" id="cours">        
+        <div class="row">
+            <figure class="col-md-4">
+            <h5>MONTPELLIER</h5>
+                <ul class="list-unstyled">
+                    <li><a href="login_mpl.php">Adultes</a></li>
+                </ul>
+            </figure>
+            <figure class="col-md-4">
+            <h5>MAUGUIO</h5>
+                <ul class="list-unstyled">  
+                    <li><a href="login_mauguio_ado2.php">Ados 2</a></li>
+                    <li><a href="login_mauguio_adultes.php">Adultes</a></li>
+                    <li><a href="login_mauguio_adultes2.php">Adultes 2</a></li>
+                </ul>
+            </figure>
+            <figure class="col-md-4">
+            <h5>LUNEL</h5>
+                <ul class="list-unstyled">
+                    <li><a href="login_lunel_ado.php">Ados</a></li>
+                    <li><a href="login_lunel_adultes.php">Adultes</a></li>
+                </ul>
+            </figure>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
